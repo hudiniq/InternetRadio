5ab91bee9514affe825019c22c8f029510129e0b
hudiniq <simon.ffs@gmail.com>
2020-04-17 14:55:40 +0200

    final
    crammed everything in a class

--- player.py
+++ player.py
@@ -8,81 +8,77 @@ import vlc
 import time
 import harvester as H
 
-root = tk.Tk()
-root.geometry("600x70")
-root.resizable(0, 0)
+def app():
+    application = InternetRadio()
 
-canvas = tk.Canvas(root, height = 70, width = 600)
-canvas.pack()
+class InternetRadio():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("600x70")
+        self.root.resizable(0, 0)
 
-url = "http://212.30.80.195:9034/rock"
-#url = "http://145.14.14.93:9034/rock"
-state = True
-play_img = ImageTk.PhotoImage(file="play.png")
-pause_img = ImageTk.PhotoImage(file="pause.png")
-play_button = None
-cover = None
-song = None
-vol = None
+        self.canvas = tk.Canvas(self.root, height = 70, width = 600)
+        self.canvas.pack()
 
-instance = vlc.Instance()
-media=instance.media_new(url)
-player=instance.media_player_new()
-player.set_media(media)
+        self.url = "http://212.30.80.195:9034/rock"
+        #self.url = "http://145.14.14.93:9034/rock"
+        self.state = True
+        self.play_img = ImageTk.PhotoImage(file="play.png")
+        self.pause_img = ImageTk.PhotoImage(file="pause.png")
+        self.play_button = None
+        self.cover = None
+        self.song = None
+        self.vol = None
 
+        self.instance = vlc.Instance()
+        self.media=self.instance.media_new(self.url)
+        self.player=self.instance.media_player_new()
+        self.player.set_media(self.media)
 
-def main():
-    global play_button, cover, song, vol
+        H.fetch()
+        self.draw()
+        
+        self.root.mainloop()
+        H.kill()
 
-    H.fetch()
-    
-    play_button = tk.Button(canvas, image = play_img, command = play_pause)
-    play_button.place(relx = 0, rely = 0, relwidth = 0.20, relheight = 1)
+    def draw(self):        
+        self.play_button = tk.Button(self.canvas, image = self.play_img, command = self.play_pause)
+        self.play_button.place(relx = 0, rely = 0, relwidth = 0.20, relheight = 1)
 
-    img = ImageTk.PhotoImage(Image.open(H.fetch_img()).resize((84, 70), Image.ANTIALIAS))
-    cover = tk.Label(canvas, image = img)
-    cover.place(relx = 0.20, relwidth = 0.14, relheight = 1)
+        self.img = ImageTk.PhotoImage(Image.open(H.fetch_img()).resize((84, 70), Image.ANTIALIAS))
+        self.cover = tk.Label(self.canvas, image = self.img)
+        self.cover.place(relx = 0.20, relwidth = 0.14, relheight = 1)
 
-    song = tk.Label(canvas, text = H.fetch_song())
-    song.place(relx = 0.34, relwidth = 0.51, relheight = 1)
+        self.song = tk.Label(self.canvas, text = H.fetch_song())
+        self.song.place(relx = 0.34, relwidth = 0.61, relheight = 1)
 
-    # refresh_button = tk.Button(canvas, text = "refresh", command = refresher)
-    # refresh_button.place(relx = 0.85, relwidth = 0.10, relheight = 1)
+        self.vol = tk.Scale(self.canvas, from_ = 100, to = 0, command=self.set_volume)
+        self.vol.set(100)
+        self.vol.place(relx = 0.93, relwidth = 0.5, relheight = 1)
 
-    vol = tk.Scale(canvas, from_ = 100, to = 0, command=set_volume)
-    vol.set(100)
-    vol.place(relx = 0.93, relwidth = 0.5, relheight = 1)
+    def play_pause(self):
+        if self.state is True:
+            self.state = False
+            self.play_button.configure(image=self.pause_img)
+            self.player.play()
+        else:
+            self.state = True
+            self.play_button.configure(image=self.play_img)
+            self.player.pause()
 
-    root.mainloop()
-    H.kill()
+    def set_volume(self, v):
+        value = self.vol.get()
+        self.player.audio_set_volume(value)
 
+    # Avtomatsko ne dela, ce ga poklices zna posodobit pesem ampak skrije sliko. 
+    # def refresher(self):
+    #     self.img = ImageTk.PhotoImage(Image.open(H.fetch_img()).resize((84, 70), Image.ANTIALIAS))
 
-def play_pause():
-    global play_button
-    global state
-    if state is True:
-        state = False
-        play_button.configure(image=pause_img)
-        player.play()
-    else:
-        state = True
-        play_button.configure(image=play_img)
-        player.pause()
-
-def set_volume(v):
-    global vol
-    value = vol.get()
-    player.audio_set_volume(value)
-
-# def refresher():
-    # global cover, song
-    # img = ImageTk.PhotoImage(Image.open(H.fetch_img()).resize((84, 70), Image.ANTIALIAS))
-
-    # cover.configure(image = img)
-    # song.configure(text = H.fetch_song())
-    
-    # root.after(1000, refresher())
+    #     self.cover.configure(image = self.img)
+    #     self.song.configure(text = H.fetch_song())
+        
+    #     self.root.after(1000, self.refresher())
 
 
 if __name__ == "__main__":
-    main()
+    app()
